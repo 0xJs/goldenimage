bce789d016a6c7ea42a16cf9e3718e924d54fb66
root <me@jonyschats.nl>
2018-11-14 06:19:07 -0500

    Fixed some bugs in the script, deleted some spaces, rewrote some comments.

--- golden_image.sh
+++ golden_image.sh
@@ -14,12 +14,13 @@ gitlog="$scriptpath/logs/log_git$(date +%Y-%m-%d_%H:%M).log"
 
 # $0 to call the name of the script
 usage="Usage: $0 [-u] [-i] [-g] [-r]
-	[-u] To update and upgrade the system and to update all github repositories listed in
-             githubrepos.txt that do exist in /opt
-	[-i] To update & upgrade and install packages listed in kaliui.txt and packages.txt 
+	[-u] To update and upgrade the system and update all github repositories
+             listed in githubrepos.txt that do exist in /opt.
+	[-i] To update & upgrade the system and install all packages listed in
+             kaliui.tx and packages.txt
 	[-g] To clone and update all github repositories in githubrepos.txt to /opt
 	[-r] To restart the system after completion of the script
-	     
+
 If you got a clean kali light system use $0 -igr"
 
 check_git (){
@@ -30,7 +31,7 @@ check_git (){
 
   if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     # -q for only redirecting errors
-    apt-get -qq install git -y &>> "$aptlog"; 
+    apt-get -qq install git -y &>> "$aptlog";
     echo Git is succesfully installed;
   else
     echo Git is already installed | tee -a "$aptlog";
@@ -42,19 +43,26 @@ apt_update (){
   echo -----------------------Updating and upgrading the system-------------------;
   echo ---------------------------------------------------------------------------;
   echo Updating;
-  # Redirect output of update to a logfile
+  # Redirect output of update to a logfile for update
   apt update -y &>> "$updatelog";
   echo Upgrading;
-  # Redirect output of upgrade to a logile
+  # Redirect output of upgrade to a logile for update
   apt full-upgrade -y &>> "$updatelog";
 }
 
-# Make directory logs in the path where the script is, if it does not exist yet
+# Print usage if no arguments where given
+if [[ $# -eq 0 ]] ; then
+    echo "$usage";
+    exit 0
+fi
+
+# Make directory named logs in the path where the script is, if it does not exist yet
 if [ ! -d $scriptpathlogs ]; then
-    cd $scriptpath 
+    cd $scriptpath
     mkdir logs
 fi
 
+# While loop for al the options
 while getopts ':uigr' opt; do
   case "$opt" in
     u)
@@ -70,19 +78,19 @@ while getopts ':uigr' opt; do
       echo ----------------Updatding GitHub Repositories in /opt----------------------;
       echo ---------------------------------------------------------------------------;
 
-      for repos in $(cat $listhpath/githubrepos.txt)
+      for repos in $(cat $listpath/githubrepos.txt)
         do
-          # tr -d '\r' for removing the ^m carriage return	
+          # tr -d '\r' for removing the ^m carriage return
           localrepodir=`echo ${repos##*/} | tr -d '\r'`
           if [ -d /opt/$localrepodir ] ; then
             cd /opt/$localrepodir;
-            echo $localrepodir >> "$gitlog"
+            echo $localrepodir >> "$gitlog";
             git pull &>> "$gitlog";
-            echo $localrepodir exists, updated tool with git pull;
+            echo $localrepodir exists, updated tool with git pull command;
           else
             echo $localrepodir does not exist, please run [-g] to clone the tools | tee -a "$gitlog";
           fi
-      done 
+      done
       ;;
     i)
      #calling the check apt_update function to have its code executed
@@ -93,14 +101,14 @@ while getopts ':uigr' opt; do
       echo -----Starting the install of the Kali UI packages listed in kaliui.txt-----;
       echo ---------------------------------------------------------------------------;
 
-      for kaliui in $(cat $listpath/kaliui.txt); 
+      for kaliui in $(cat $listpath/kaliui.txt);
        do
          if [ $(dpkg-query -W -f='${Status}' $kaliui 2>/dev/null | grep -c "ok installed") -eq 0 ] ; then
            # Debian_frontend... for not having to click yes & -q for only redirecting errors
            # If the apt install exits with not 0 (not succeeded) it will print installation failed
              DEBIAN_FRONTEND=noninteractive apt -qq install $kaliui -y &>> "$aptlog"\
 		   && echo $kaliui is succesfully installed | tee -a "$aptlog"\
-		   || echo $kaliui Installation failed | tee -a "$aptlog"
+		   || echo $kaliui Installation failed | tee -a "$aptlog";
          else
            echo $kaliui is already installed | tee -a "$aptlog";
          fi
@@ -137,11 +145,11 @@ while getopts ':uigr' opt; do
 
       for repos in $(cat $listpath/githubrepos.txt)
         do
-          # tr -d '\r' for removing the ^m carriage return	
+          # tr -d '\r' for removing the ^m carriage return
           localrepodir=`echo ${repos##*/} | tr -d '\r'`
           if [ -d /opt/$localrepodir ] ; then
             cd /opt/$localrepodir;
-            echo $localrepodir >> "$gitlog"
+            echo $localrepodir >> "$gitlog";
             git pull &>> "$gitlog";
             echo $localrepodir already exists, updated tool with git pull;
           else
@@ -156,15 +164,14 @@ while getopts ':uigr' opt; do
       echo ---------------------------------------------------------------------------;
       echo ----------------Restarting the system in 10 seconds------------------------;
       echo ---------------------------------------------------------------------------;
-      sleep 10      
-      reboot 
+      sleep 10;
+      reboot;
       ;;
     h)
-      echo "$usage"
+      echo "$usage";
       ;;
     \?)
-      echo "$usage"
+      echo "$usage";
       ;;
   esac
 done
-	    
